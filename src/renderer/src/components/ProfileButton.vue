31c2e950a408eb8d2cb0ad2f423ff7329295a777
Didiloy <didiloy2001@gmail.com>
2025-11-18 17:09:21 +0100

    changed background of profile button when selected

--- src/renderer/src/components/ProfileButton.vue
+++ src/renderer/src/components/ProfileButton.vue
@@ -45,22 +45,17 @@ const goHome = (): void => {
     padding: 1.5px;
 }
 
+
+
 .profile-rainbow.is-active {
     --border-angle: 0deg;
     animation: border-angle-rotate 3s infinite linear;
     background: conic-gradient(
         from var(--border-angle),
-        oklch(100% 0.37 0),
-        oklch(100% 0.37 45),
-        oklch(100% 0.37 90),
-        oklch(100% 0.37 135),
-        oklch(100% 0.37 180),
-        oklch(100% 0.37 225),
-        oklch(100% 0.37 270),
-        oklch(100% 0.37 315),
-        oklch(100% 0.37 360)
+        #4a84ff, #8a5cf7, #d46eff, #4a84ff
     );
 }
+
 
 @keyframes border-angle-rotate {
     from {
