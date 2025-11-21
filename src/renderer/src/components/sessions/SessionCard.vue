42e02754d5f727e97edae297d6bbecd61fd6cbb8
Natour-Mazen <mazen.natour@gmail.com>
2025-11-21 14:22:51 +0100

    Update SessionCard.vue

--- src/renderer/src/components/sessions/SessionCard.vue
+++ src/renderer/src/components/sessions/SessionCard.vue
@@ -54,10 +54,10 @@ function getParticipantTooltip(
     >
         <div
             v-if="session.activity.banner"
-            class="absolute inset-1 rounded-2xl bg-cover bg-center bg-no-repeat"
+            class="absolute inset-1 rounded-2xl bg-cover bg-center bg-no-repeat bg-fixed"
             :style="{ backgroundImage: `url(${session.activity.banner})` }"
-        ></div>
-        <div class="absolute inset-0 bg-surface-100/65 backdrop-blur-3xl rounded-2xl"></div>
+        />
+        <div class="absolute inset-0 bg-surface-100/55 backdrop-blur-3xl rounded-2xl"/>
         <!-- Banner -->
         <div class="relative h-20 w-full overflow-hidden z-10">
             <!-- Logo + Title -->
