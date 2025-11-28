90bfd9686f2b440d0744b8028671c003013f2008
Natour-Mazen <mazen.natour@gmail.com>
2025-11-28 16:08:50 +0100

    Refactor i18n keys and remove debug logs
    Updated i18n key references for consistency and clarity across ActivityProfile, Home, and LoginOrSignup views. Removed unused imports and debug console logs from ActivityProfile and Home components to clean up code.

--- src/renderer/src/components/app-updates/Versions.vue
+++ src/renderer/src/components/app-updates/Versions.vue
@@ -2,16 +2,13 @@
 import { reactive } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { t } = useI18n()
 const versions = reactive({ ...window.electron.process.versions })
 </script>
 
 <template>
     <ul class="versions">
-        <li class="electron-version">
-            versions electron {{  versions.electron  }}
-        </li>
-        <li class="chrome-version">versions chromium {{ versions.chrome  }}</li>
+        <li class="electron-version">versions electron {{ versions.electron }}</li>
+        <li class="chrome-version">versions chromium {{ versions.chrome }}</li>
         <li class="node-version">versions node {{ versions.node }}</li>
     </ul>
 </template>
